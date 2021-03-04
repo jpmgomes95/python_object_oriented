9cd4d294e2902a295a4c702ace0cbc3ebd7a5980
Jotape <jpmgomes95@gmail.com>
2021-03-04 19:28:32 -0300

    Completely translated to engligh now

--- test.py
+++ test.py
@@ -1,7 +1,4 @@
 
 def create_account(number, owner, balance, limit):
-    conta = {"number": number,
-             "owner": owner,
-             "balance": balance,
-             "limit": limit}
-    return conta
+    count = {"number": number, "owner": owner, "balance": balance, "limit": limit}
+    return count
