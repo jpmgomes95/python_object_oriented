469231eba789bf6fa9dd53221169d56e5eeff5d6
João Pedro Moura Gomes <jpmgomes95@gmail.com>
2023-03-19 02:08:08 -0300

    bug fixes not much else

--- test.py
+++ test.py
@@ -1,18 +1,18 @@
 
- def create_account(number, owner, balance, limit):
+def create_account(number, owner, balance, limit):
      account = {"number": number, "owner": owner, "balance": balance, "limit": limit}
      return account
 
 
- def deposit(account, value):
+def deposit(account, value):
      account["balance"] += value
 
 
- def withdraw(account, value):
+def withdraw(account, value):
      account["balance"] -= value
 
 
- def bank_statement(account):
+def bank_statement(account):
      print("Your bank balance is: {}".format(account["balance"]))
 
 # ----------------------------------------
